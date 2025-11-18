26ddc7a3d5f216c12547bd5ff2ec3bbd18c59ef1
kolk-torg <thordur.grimsson@gmail.com>
2025-11-18 11:57:32 +0000

    Add _ref to gitignore and remove from tracking

--- _ref/penpot-develop/frontend/src/app/render_wasm/wasm.cljs
+++
@@ -1,6 +0,0 @@
-(ns app.render-wasm.wasm)
-
-(defonce internal-frame-id nil)
-(defonce internal-module #js {})
-(defonce serializers #js {})
-(defonce context-initialized? false)
--- _ref/penpot-develop/render-wasm/src/js/wapi.js
+++
@@ -1,8 +0,0 @@
-addToLibrary({
-  wapi_requestAnimationFrame: function wapi_requestAnimationFrame() {
-    return window.requestAnimationFrame(Module._process_animation_frame);
-  },
-  wapi_cancelAnimationFrame: function wapi_cancelAnimationFrame(frameId) {
-    return window.cancelAnimationFrame(frameId);
-  }
-});
